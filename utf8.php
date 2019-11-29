c1811b28137a0b5a71737ea09397e69315617c2e
Hill <hill@kimo.com>
2019-11-29 07:27:11 +0000

    Add decodeUtf16

--- utf8.php
+++ utf8.php
@@ -17,11 +17,27 @@ class utf8 extends PlugIn
       }
     }
 
-    public function substr($str, $start, $length=null, $encoding=null) {
+    public function internalEncoding($encoding=null) {
       $encoder = \PMVC\plug($this['encoder']);
       if (is_null($encoding)) {
-        $encoding = $encoder->internalEncoding(); 
+        $encoding = $encoder->internalEncoding();
+      } else {
+        $encoding = $encoder->internalEncoding($encoding); 
       }
+      return $encoding;
+    }
+
+    public function convertEncoding($val, $to_encoding, $from_encoding=null) {
+      $from_encoding = $this->internalEncoding($from_encoding);
+      return \PMVC\plug($this['encoder'])->convertEncoding($val, $to_encoding, $from_encoding);
+    }
+
+    public function decodeUtf16($str) {
+      return \PMVC\plug($this['encoder'])->convertEncoding($val, $to_encoding, 'UTF-16LE');
+    }
+
+    public function substr($str, $start, $length=null, $encoding=null) {
+      $encoding = $this->internalEncoding($encoding);
       return \PMVC\plug($this['encoder'])->substr($str, $start, $length, $encoding);
     }
 }
