2ea5c4ee7482d715f3cf643a23892a5eccfcc97a
Hill <hill@kimo.com>
2019-12-05 19:20:50 +0000

    fixed infinity debug

--- utf8.php
+++ utf8.php
@@ -8,15 +8,23 @@ ${_INIT_CONFIG}[_CLASS] = __NAMESPACE__.'\utf8';
 
 class utf8 extends PlugIn
 {
+    private $_encoder;
+
     public function init()
     {
       if (!$this['encoder']) {
         $this['encoder'] = 'mbstring';
       }
+      $this->setEncoder();
+    }
+
+    public function setEncoder()
+    {
+      $this->_encoder = \PMVC\plug($this['encoder']);
     }
 
     public function internalEncoding($encoding=null, $keepDefault=false) {
-      $encoder = \PMVC\plug($this['encoder']);
+      $encoder = $this->_encoder;
       $default = $encoder->internalEncoding();
       if (is_null($encoding)) {
         $encoding = $default;
@@ -31,16 +39,16 @@ class utf8 extends PlugIn
 
     public function convertEncoding($val, $to_encoding, $from_encoding=null) {
       $from_encoding = $this->internalEncoding($from_encoding, true);
-      return \PMVC\plug($this['encoder'])->convertEncoding($val, $to_encoding, $from_encoding);
+      return $this->_encoder->convertEncoding($val, $to_encoding, $from_encoding);
     }
 
     public function decodeUtf16($str, $to_encoding=null) {
       $to_encoding = $this->internalEncoding($to_encoding, true);
-      return \PMVC\plug($this['encoder'])->convertEncoding($str, $to_encoding, 'UTF-16LE');
+      return $this->_encoder->convertEncoding($str, $to_encoding, 'UTF-16LE');
     }
 
     public function substr($str, $start, $length=null, $encoding=null) {
       $encoding = $this->internalEncoding($encoding, true);
-      return \PMVC\plug($this['encoder'])->substr($str, $start, $length, $encoding);
+      return $this->_encoder->substr($str, $start, $length, $encoding);
     }
 }
