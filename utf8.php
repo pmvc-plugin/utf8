061b1730378e50d9583fdd15d2cb01b67b4eb6b4
Hill <hill@kimo.com>
2021-04-15 08:48:12 +0800

    improve toUtf8 with is_object

--- utf8.php
+++ utf8.php
@@ -4,8 +4,7 @@ namespace PMVC\PlugIn\utf8;
 
 use PMVC\PlugIn;
 
-${_INIT_CONFIG
-}[_CLASS] = __NAMESPACE__.'\utf8';
+${_INIT_CONFIG}[_CLASS] = __NAMESPACE__ . '\utf8';
 
 class utf8 extends PlugIn
 {
@@ -24,49 +23,61 @@ class utf8 extends PlugIn
         $this->_encoder = \PMVC\plug($this['encoder']);
     }
 
-    public function toUtf8($val, $from_encoding_list = null)
+    public function toUtf8($val, $from_encoding_list = null, $bInit = false)
     {
-        $isObj = is_object($val);
+        $isObj = is_object($val) && !$bInit;
         if ($isObj || \PMVC\isArray($val)) {
             $myval = \PMVC\get($val);
-            array_walk_recursive(
-                $myval, function (&$item) use ($from_encoding_list) {
-                    if (is_string($item)) {
-                        if (!$this->detectEncoding($item, 'utf-8', true)) {
-                          $item = $this->convertEncoding($item, 'utf-8', $from_encoding_list);
-                        }
-                    } else {
-                      $item = $this->toUtf8($item, $from_encoding_list);
+            array_walk_recursive($myval, function (&$item) use (
+                $from_encoding_list
+            ) {
+                if (is_string($item)) {
+                    if (!$this->detectEncoding($item, 'utf-8', true)) {
+                        $item = $this->convertEncoding(
+                            $item,
+                            'utf-8',
+                            $from_encoding_list
+                        );
                     }
+                } else {
+                    $item = $this->toUtf8($item, $from_encoding_list, true);
                 }
-            );
+            });
             if ($isObj) {
-                $myval = (object)$myval;
+                $myval = (object) $myval;
             }
             return $myval;
-        } elseif (is_string($val) ) {
+        } elseif (is_string($val)) {
             if (!$this->detectEncoding($val, 'utf-8', true)) {
-              return $this->convertEncoding($val, 'utf-8', $from_encoding_list);
+                return $this->convertEncoding(
+                    $val,
+                    'utf-8',
+                    $from_encoding_list
+                );
             } else {
-              return $val;
+                return $val;
             }
-        } elseif (is_numeric($val) ) { 
+        } elseif (is_numeric($val)) {
             return $val;
         } else {
             $myval = trim(var_export($val, true));
             if (!$this->detectEncoding($myval, 'utf-8', true)) {
-              $myval = $this->convertEncoding($myval, 'utf-8', $from_encoding_list);
+                $myval = $this->convertEncoding(
+                    $myval,
+                    'utf-8',
+                    $from_encoding_list
+                );
             }
             return $myval;
         }
     }
 
-    public function detectOrder($encoding_list = null, $keepDefault=false) 
+    public function detectOrder($encoding_list = null, $keepDefault = false)
     {
         $encoder = $this->_encoder;
         $default = $encoder->detectOrder();
         if (is_null($encoding_list)) {
-            $encoding_list = $default; 
+            $encoding_list = $default;
         } else {
             $isSuccessful = $encoder->detectOrder($encoding_list);
             if (!$isSuccessful) {
@@ -78,7 +89,7 @@ class utf8 extends PlugIn
         return $encoding_list;
     }
 
-    public function detectEncoding($str, $encoding_list = null, $strict = false) 
+    public function detectEncoding($str, $encoding_list = null, $strict = false)
     {
         if (is_null($encoding_list)) {
             $encoding_list = $this->detectOrder();
@@ -86,7 +97,7 @@ class utf8 extends PlugIn
         return $this->_encoder->detectEncoding($str, $encoding_list, $strict);
     }
 
-    public function internalEncoding($encoding=null, $keepDefault=false) 
+    public function internalEncoding($encoding = null, $keepDefault = false)
     {
         $encoder = $this->_encoder;
         $default = $encoder->internalEncoding();
@@ -103,27 +114,43 @@ class utf8 extends PlugIn
         return $encoding;
     }
 
-    public function convertEncoding($val, $to_encoding=null, $from_encoding=null) 
-    {
+    public function convertEncoding(
+        $val,
+        $to_encoding = null,
+        $from_encoding = null
+    ) {
         $to_encoding = $this->internalEncoding($to_encoding, true);
         $from_encoding = $this->detectEncoding($val, $from_encoding, true);
-        return $this->_encoder->convertEncoding($val, $to_encoding, $from_encoding);
+        return $this->_encoder->convertEncoding(
+            $val,
+            $to_encoding,
+            $from_encoding
+        );
     }
 
-    public function decodeUtf16($str, $to_encoding=null) 
+    public function decodeUtf16($str, $to_encoding = null)
     {
         $to_encoding = $this->internalEncoding($to_encoding, true);
         return $this->_encoder->convertEncoding($str, $to_encoding, 'UTF-16LE');
     }
 
-    public function substr($str, $start, $length=null, $encoding=null) 
+    public function substr($str, $start, $length = null, $encoding = null)
     {
         $encoding = $this->internalEncoding($encoding, true);
         return $this->_encoder->substr($str, $start, $length, $encoding);
     }
 
-    public function eregReplace($pattern, $replacement, $string, $option='msr') 
-    {
-        return $this->_encoder->eregReplace($pattern, $replacement, $string, $option);
+    public function eregReplace(
+        $pattern,
+        $replacement,
+        $string,
+        $option = 'msr'
+    ) {
+        return $this->_encoder->eregReplace(
+            $pattern,
+            $replacement,
+            $string,
+            $option
+        );
     }
 }
